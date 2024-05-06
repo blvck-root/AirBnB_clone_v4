94314adf7ce557f961a4276371b4249f5d40fb3d
blvck-root <mtndlovu81@gmail.com>
2024-05-06 17:27:08 +0200

    add task 5: filter places by amenity

---
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -0,0 +1,55 @@
+$(document).ready(function () {
+  const amenities = {};
+  $(document).on('change', "input[type='checkbox']", function () {
+    if (this.checked) {
+      amenities[$(this).data('id')] = $(this).data('name');
+    } else {
+      delete amenities[$(this).data('id')];
+    }
+    const list = Object.values(amenities);
+    if (list.length > 0) {
+      $('div.amenities h4').text(Object.values(amenities).join(', '));
+    } else {
+      $('div.amenities h4').html('&nbsp');
+    }
+  });
+
+  $.get('http://127.0.0.1:5001/api/v1/status/', function (data, textStatus) {
+    if (data.status === 'OK') {
+      $('div#api_status').addClass('available');
+    } else {
+      $('div#api_status').removeClass('available');
+    }
+  });
+
+  $.ajax({
+    type: 'POST',
+    url: 'http://127.0.0.1:5001/api/v1/places_search',
+    data: '{}',
+    dataType: 'json',
+    contentType: 'application/json',
+    success: function (data) {
+      for (let i = 0; i < data.length; i++) {
+        let place = data[i];
+        $('.places ').append('<article><h2>' + place.name + '</h2><div class="price_by_night"><p>$' + place.price_by_night + '</p></div><div class="information"><div class="max_guest"><div class="guest_image"></div><p>' + place.max_guest + '</p></div><div class="number_rooms"><div class="bed_image"></div><p>' + place.number_rooms + '</p></div><div class="number_bathrooms"><div class="bath_image"></div><p>' + place.number_bathrooms + '</p></div></div><div class="description"><p>' + place.description + '</p></div></article>');
+      }
+    }
+  });
+
+  $('.filters > button').click(function () {
+    $('.places > article').remove();
+    $.ajax({
+      type: 'POST',
+      url: 'http://127.0.0.1:5001/api/v1/places_search',
+      data: JSON.stringify({'amenities': Object.keys(checkedAmenities)}),
+      dataType: 'json',
+      contentType: 'application/json',
+      success: function (data) {
+        for (let i = 0; i < data.length; i++) {
+          let place = data[i];
+          $('.places ').append('<article><h2>' + place.name + '</h2><div class="price_by_night"><p>$' + place.price_by_night + '</p></div><div class="information"><div class="max_guest"><div class="guest_image"></div><p>' + place.max_guest + '</p></div><div class="number_rooms"><div class="bed_image"></div><p>' + place.number_rooms + '</p></div><div class="number_bathrooms"><div class="bath_image"></div><p>' + place.number_bathrooms + '</p></div></div><div class="description"><p>' + place.description + '</p></div></article>');
+        }
+      }
+    });
+  });
+});
